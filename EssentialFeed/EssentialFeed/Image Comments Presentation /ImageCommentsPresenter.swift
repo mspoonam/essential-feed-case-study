56c22de25b2a569893574071bd4c9a5f78d3194d
mspoonam <mspoonam.learning@gmail.com>
2022-12-30 22:41:09 -0500

    Add image comments title

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation /ImageCommentsPresenter.swift
@@ -0,0 +1,17 @@
+//	
+// Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+
+public final class ImageCommentsPresenter {
+    
+    public static var title: String {
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+                          tableName: "ImageComments",
+                          bundle: Bundle(for: Self.self),
+                          comment: "Title for the feed view")
+    }
+    
+}
---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresentationTests.swift
@@ -0,0 +1,26 @@
+//	
+// Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsPresentationTests: XCTestCase {
+    
+    func test_title_isLocalized() {
+        XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+    }
+    
+    // MARK: - Helpers
+
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table: String = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+    
+}
