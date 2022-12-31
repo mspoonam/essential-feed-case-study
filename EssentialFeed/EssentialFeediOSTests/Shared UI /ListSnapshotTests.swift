2b3c7b8648145e211de396f93e33a7e50322856a
mspoonam <mspoonam.learning@gmail.com>
2022-12-31 08:40:57 -0500

    Extract shared list snapshots

---
+++ EssentialFeed/EssentialFeediOSTests/Shared UI /ListSnapshotTests.swift
@@ -0,0 +1,34 @@
+//	
+// Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeediOS
+import EssentialFeed
+
+class ListSnapshots: XCTestCase {
+    func test_emptyFeed() {
+        let sut = makeSUT()
+
+        sut.display(emptyFeed())
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
+    }
+    
+    // MARK: - Helpers
+
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyFeed() -> [FeedImageCellController] {
+        return []
+    }
+}
